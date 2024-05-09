0fc64ab2e6ec0429769a127e2d29bc6573052529
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-05-09 22:17:45 +0700

    update

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+ConnectionService cn = new ConnectionService();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
@@ -34,45 +36,5 @@ app.MapControllerRoute(
     pattern: "system/*",
     defaults: new { controller = "System", action = "Index" }
 );
-/*
-ConnectionService connectionService = new ConnectionService();
 
-if (connectionService.cn != null)
-{
-    connectionService.cn.Open();
-    try
-    {
-        // Tạo truy vấn SQL để lấy dữ liệu từ bảng admin
-        string query = "SELECT * FROM Products LEFT JOIN [All-code] ON Products.location_id = [All-code].id ";
-
-        // Tạo một SqlCommand để thực thi truy vấn
-        SqlCommand command = new SqlCommand(query, connectionService.cn);
-
-        // Thực thi truy vấn và nhận một SqlDataReader để đọc dữ liệu
-        SqlDataReader reader = command.ExecuteReader();
-
-        // Duyệt qua từng dòng trong kết quả và xử lý dữ liệu
-        while (reader.Read())
-        {
-            // Ví dụ: Đọc dữ liệu từ mỗi cột và in ra console
-            string adminId = reader["id"].ToString();
-            string username = reader["model"].ToString();
-            string password = reader["content_title"].ToString();
-            Console.WriteLine($"AdminId: {adminId}, Username: {username}, Password: {password}");
-        }
-
-        // Đóng SqlDataReader sau khi hoàn thành
-        reader.Close();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Error: " + ex.ToString());
-    }
-    finally
-    {
-        // Đóng kết nối sau khi sử dụng xong
-        connectionService.cn.Close();
-    }
-}
-*/
 app.Run();
