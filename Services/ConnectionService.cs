d8a7b87cb76f3174fab1728f04f8dd04bedca51e
Strikerzzzz <strikezz2003@gmail.com>
2024-05-09 22:02:55 +0700

    connectDB

---
+++ Services/ConnectionService.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+public class ConnectionService
+{
+    public readonly SqlConnection? cn;
+
+    public ConnectionService()
+    {
+        string connectionString = "server=(local);database=manage-rent-bike;Integrated Security=True";
+
+        try
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            this.cn = connection;
+            // Thực hiện các thao tác với cơ sở dữ liệu ở đây
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("", e.ToString());
+        }
+    }
+}
