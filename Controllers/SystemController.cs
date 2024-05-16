250430ed041642ff915d8d49c31dd0b2468e780f
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-05-16 23:06:38 +0700

    update

--- Controllers/SystemController.cs
+++ Controllers/SystemController.cs
@@ -1,10 +1,34 @@
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webASP.Controllers;
 public class SystemController : Controller
 {
+
+    private readonly ILogger<SystemController> _logger;
+    private readonly SqlConnection? connection;
+
+    public SystemController(ILogger<SystemController> logger)
+    {
+        // _logger = logger;
+        // ConnectionService cnService = new ConnectionService();
+        // this.connection = cnService.cn;
+
+    }
+
     public IActionResult Index()
     {
         return View();
     }
+
+
+    [HttpPost]
+    public String HandleLogin(string email, string password)
+    {
+        Console.WriteLine(email);
+        Console.WriteLine(password);
+        return email;
+    }
+
+
 }
