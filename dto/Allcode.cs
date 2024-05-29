aeae4b3e833320b9bdbba1ed31e3d60ffaa5a343
Thanh-Jinn <99498069+Thanh-Jinn@users.noreply.github.com>
2024-05-29 23:34:52 +0700

    COMMIT

--- dto/Allcode.cs
+++ dto/Allcode.cs
@@ -1,20 +1,19 @@
-public class ALlcodeDTO
+
+public class AllCodeDTO
 {
-
     public readonly int id;
-    public readonly string type;
-    public readonly string content_title;
-    public readonly string content_detail;
-    public readonly string code;
-
-
-    public ALlcodeDTO(int id, string type, string content_title, string content_detail, string code)
+    public readonly string? type;
+    public readonly string? content_title;
+    public readonly string? content_detail;
+    public readonly string? code;
+    public AllCodeDTO() { }
+    public AllCodeDTO(int id, string type, string content_title, string content_detail, string code)
     {
         this.id = id;
         this.type = type;
         this.content_title = content_title;
         this.content_detail = content_detail;
         this.code = code;
-
     }
+
 }
